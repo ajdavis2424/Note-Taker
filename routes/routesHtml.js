eae1afa1b48f296e7f39cf4ca92fc3f59c1ac0c6
ALexander Davis <ajdavis2424@gmail.com>
2021-10-26 15:47:22 -0400

    changing routes

---
+++ routes/routesHtml.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const router = require('express').Router();
+
+// FUll SCRAP
+// '/notes/ responds with the notes.html file via bootcamp starter code
+router.get('/notes', (req, res) => {
+  
+    //this finds the path
+  res.sendFile(path.join(__dirname, '../public/notes.html'));
+});
+
+router.get('*', (req, res) =>
+    res.sendFile(path.join(__dirname, '../public/index.html'))
+);
+
+// This allows new routes file to be exported
+module.exports = router;

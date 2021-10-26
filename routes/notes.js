9123dab70bcc8848aa57938816f765a8b6748bfd
ALexander Davis <ajdavis2424@gmail.com>
2021-10-26 15:10:56 -0400

    scrapping, cannot debug

--- routes/notes.js
+++ routes/notes.js
@@ -1,48 +1,52 @@
 const notes = require('express').Router();
-const { readFromFile, readAndAppend, writeToFile } = require('../helpers/fsUtils');
+const fs = require('fs');
+
+// This brings in helper fsUtilsfor reading and appending to .=db.json file, may add delete
+const { readFromFile, readAndAppend,} = require('../helpers/fsUtils');
+
+// This will bring in uuid module to create "unique" user id
 const uuid = require('../helpers/uuid');
 
 // THIS IS THE GET ROUTE FOR SAVED NOTED
-notes.get('/', (req, res) =>
-  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
-);
+notes.get('/', (req, res) => {
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
 
 // THIS IS THE POST ROUTE FOR SAVING NOTES
 notes.post('/', (req, res) => {
-  
     const { title, text } = req.body;
-  
-    if (req.body) {
-      const newNote = {
+  // new user ID created
+    const id = uuid();
+    const newNotes = {
         title,
         text,
-        id: uuid(),
+        id:
       };
-  
-      readAndAppend(newNote, './db/db.json');
-      res.json(`This note has been saved successfully!!`);
-    } else {
-      res.error('THERE WAS AN ERROR SAVING THIS NOTE');
-    }
-});
-
-// THIS IS THE POST ROUTE FOR DELETING NOTES
-notes.delete('/:id', async (req, res) => {
-
-  const { id } = req.params;
-
-  if (id) {
     
-    const currentNotes = await readFromFile('./db/db.json').then((data) => JSON.parse(data));    
-    
-    const updatedNotes = currentNotes.filter(note => note.id !== id);
+      readAndAppend(newNotes, './db/db.json');
+      res.send(`This note has been saved successfully!!`);
+    // } else {
+    //   res.error('THERE WAS AN ERROR SAVING THIS NOTE');
+    // }
+})
 
-    writeToFile('./db/db.json', updatedNotes);
-    res.json('THIS NOTE HAS BEEN DELETED');
-  } else {
-    res.error('ERROR DELETING THIS NOTE');
-  }
-});
+// // THIS IS THE POST ROUTE FOR DELETING NOTES
+// notes.delete('/:id', async (req, res) => {
+
+//   const { id } = req.params;
+
+//   if (id) {
+    
+//     const currentNotes = await readFromFile('./db/db.json').then((data) => JSON.parse(data));    
+    
+//     const updatedNotes = currentNotes.filter(note => note.id !== id);
+
+//     writeToFile('./db/db.json', updatedNotes);
+//     res.json('THIS NOTE HAS BEEN DELETED');
+//   } else {
+//     res.error('ERROR DELETING THIS NOTE');
+//   }
+// });
 
 // module.exports allow us to use this file elseswhere 
 module.exports = notes;
